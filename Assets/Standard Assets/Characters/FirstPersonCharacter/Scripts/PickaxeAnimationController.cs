427e38fe2286d3d1b509ac08bc088904c840acb8
Nalin Ranjan <nalin.ranjan@outlook.com>
2018-01-28 10:24:13 -0500

    First commit. Added Unity Project

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PickaxeAnimationController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickaxeAnimationController : MonoBehaviour {
+
+	private Animation attackAnimation;
+	private ParticleSystem hitParticles;
+	private GameObject overlay;
+	private bool newAttack;
+
+	private void Start() {
+		attackAnimation = GetComponent<Animation> ();
+		hitParticles = GetComponentInChildren<ParticleSystem> ();
+		newAttack = false;
+		overlay = GameObject.Find ("WinOverlay");
+		overlay.SetActive (false);
+	}
+
+	public void attack() {
+		newAttack = true;
+		attackAnimation.Play ();
+	}
+
+	private void OnCollisionEnter (Collision col) {
+		print ("Collided");
+		if (attackAnimation.isPlaying && col.gameObject.name == "JohnWall") {
+			Destroy (col.gameObject);
+		}
+	}
+
+	public void collide (GameObject obj) {
+		if (attackAnimation.isPlaying && newAttack) {
+			hitParticles.Play ();
+			if (obj.GetComponent<WallController> ()) {
+				obj.GetComponent<WallController> ().hit ();
+				StartCoroutine (WinCondition ());
+			}
+			newAttack = false;
+		}
+	}
+
+	private IEnumerator WinCondition() {
+		yield return new WaitForSeconds (0.1f);
+
+		if (GameObject.FindGameObjectsWithTag ("Wall").Length == 0) {
+			overlay.SetActive (true);
+		}
+	}
+}
---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/WallController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+
+	private Renderer renderer;
+	private int health;
+
+	void Start () {
+		health = 2;
+		renderer = gameObject.GetComponent<Renderer> ();
+	}
+
+	public void hit() {
+		health--;
+		if (health == 0) {
+			Destroy (gameObject, 0.1f);
+		}
+		renderer.material.SetColor ("_Color", Color.gray);
+	}
+}
